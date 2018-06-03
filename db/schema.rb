895bfdc5c52d6f81be0e3ba40f317b5c809dc013
Balázs Koskóci <balazs.koskoci@vodafone.com>
2018-06-03 23:41:22 +0100

    Posts scaffold with ActiveStorage setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_03_201016) do
+ActiveRecord::Schema.define(version: 2018_06_03_201231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 2018_06_03_201016) do
   create_table "counters", force: :cascade do |t|
     t.string "name"
     t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
