de03bb2678377e4f217e79a31423a65a40993a0f
Balázs Koskóci <balazs.koskoci@vodafone.com>
2018-07-22 19:09:00 +0100

    Tweak test endpoint

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    counter = Counter.find_or_create_by(name: "default")
-    counter.value = (counter.value || 0) + 1
-    counter.save!
-    render plain: "Fine New Counter #{counter.value}; #{Rails.version}/#{RUBY_VERSION}"
+    @counter = Counter.find_or_create_by(name: "default")
+    @counter.value = (@counter.value || 0) + 1
+    @counter.save!
+    # render plain: "Fine New Counter #{counter.value}; #{Rails.version}/#{RUBY_VERSION}"
+    render "welcome/index"
   end
 end
