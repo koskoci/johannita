29a58c06c8cde3187905ac59118a313eaa3f12d8
Balázs Koskóci <balazs.koskoci@vodafone.com>
2018-08-09 20:16:11 +0100

    Add name to user

---
+++ db/migrate/20180809172124_add_name_to_user.rb
@@ -0,0 +1,7 @@
+class AddNameToUser < ActiveRecord::Migration[5.2]
+  def change
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE users CASCADE")
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+  end
+end
