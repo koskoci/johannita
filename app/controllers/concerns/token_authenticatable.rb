2f05808f7c15095c24bf6bd1ff4967d71652e76d
Balázs Koskóci <balazs.koskoci@vodafone.com>
2018-08-09 21:00:28 +0100

    Fix error messages

--- app/controllers/concerns/token_authenticatable.rb
+++ app/controllers/concerns/token_authenticatable.rb
@@ -8,13 +8,17 @@ module TokenAuthenticatable
 
     before_action :authenticate_user
 
-    rescue_from NotAuthneticatedException, with: -> { render json: { error: 'Not Authenticated' }, status: 401 }
+    rescue_from NotAuthneticatedException, with: -> { render json: { error: command.errors }, status: 401 }
   end
 
   private
 
   def authenticate_user
-    command = Authentication::DecodeAuthenticationCommand.call(request.headers)
-    command.errors || @current_user = command.result
+    @current_user = command.result
+    raise NotAuthneticatedException unless @current_user
+  end
+
+  def command
+    @_command ||= Authentication::DecodeAuthenticationCommand.call(request.headers)
   end
 end
