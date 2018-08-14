e7af7e20d657c28a45c5a5e5f1f3c9e6c4ff588c
Balázs Koskóci <balazs.koskoci@vodafone.com>
2018-08-14 21:25:45 +0100

    Add host to image url

--- app/serializable/serializable_post.rb
+++ app/serializable/serializable_post.rb
@@ -1,10 +1,12 @@
 class SerializablePost < JSONAPI::Serializable::Resource
+  include Rails.application.routes.url_helpers
+
   type 'posts'
   attribute :title
   attribute :content
   attribute :created_at
   attribute :updated_at
   attribute :image_url do
-    @url_helpers.url_for(@object.image) if @object.image.attached?
+    url_for(@object.image) if @object.image.attached?
   end
 end
