1e2b8903f45b23714132cfcbfcc52f0b0f12b5c9
Balázs Koskóci <balazs.koskoci@vodafone.com>
2018-08-24 22:19:27 +0100

    Add date validation to user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :pav_until_before_type_cast, date: true
+  validates :driving_licence_since_before_type_cast, date: true
 end
