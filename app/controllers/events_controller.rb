be9c06c9224d17e5864c550261955b4282189795
Balázs Koskóci <balazs.koskoci@vodafone.com>
2018-08-25 13:47:36 +0100

    Remove unneeded

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -64,15 +64,6 @@ class EventsController < ApplicationController
     end
   end
 
-  # GET /events/new
-  def new
-    @event = Event.new
-  end
-
-  # GET /events/1/edit
-  def edit
-  end
-
   # POST /events
   def create
     authorize!
