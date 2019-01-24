98eba9b4cf84605e7ca4bc34d19ddd2f1e04feea
Zoltan Humpok <info@zoltan.pro>
2019-01-24 20:55:57 +0100

    Add admin navigation

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,41 +1,34 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
-    </v-toolbar>
-
+    <div class="nav" v-if=isAdminRoute>
+      <AdminNavigation />
+    </div>
     <v-content>
       <router-view/>
     </v-content>
-
     <Footer/>
   </v-app>
 </template>
 
 <script>
 import Footer from './components/Footer.vue';
+import AdminNavigation from './components/admin/Navigation.vue';
 
 export default {
   name: 'App',
   components: {
     Footer,
+    AdminNavigation,
   },
   data() {
     return {
       //
     };
+  },
+  computed: {
+    isAdminRoute() {
+      return this.$route.path.startsWith('/admin');
+    },
   },
 };
 </script>
--- frontend-vue/src/router.js
+++ frontend-vue/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import AdminPosts from './components/admin/Posts.vue';
+import AdminPost from './components/admin/Post.vue';
 
 Vue.use(Router);
 
@@ -12,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/admin/posts',
+      name: 'admin-posts',
+      component: AdminPosts,
+    },
+    {
+      path: '/admin/posts/:id/edit',
+      name: 'admin-posts-edit',
+      component: AdminPost,
     },
     {
       path: '/about',
